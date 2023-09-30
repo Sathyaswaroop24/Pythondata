d69cad965a66b3762983d4cd328881f180dc4d82
raahoolkumeriya <kumeriya_rahul@rediffmail.com>
2023-09-30 13:08:32 +0530

    New version:

---
+++ 04-Numbers-Strings/test_strings.py
@@ -0,0 +1,48 @@
+"""
+__version__ :   1.0.0
+__author__  : codelocked
+__desc__    : Sample python file for string tutorial 
+__date__    : Since 2023
+
+__changelog__:
+    date        who        version   description
+    30/09/23    codelocked  1.0.0   Draft version
+"""
+'''
+message = "Codelocked"
+
+print(message[:4] + 'Un' + message[4:] )
+
+print(message * 3)
+
+print(message + '----' + message)
+
+'''
+'''
+
+print('*'* 40)
+print("This is Shree Ram mandir")
+print('*'* 40)
+print("laddu    1000")
+print('*'* 40)
+print(f"Total : { 1000 + 1000 }")
+print('*'* 40)
+'''
+
+'''
+
+message = "This is random string of tutorial"
+if 'random1' in message:
+    print('Found')
+else:
+    print("Not found")
+
+print('random1' in message)
+
+'''
+
+message = R"This is random \t string of \ntutorial"
+
+print(message)
+
+print(u'C:\\windows\\directory\\path')
